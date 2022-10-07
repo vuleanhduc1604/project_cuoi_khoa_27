6d93de19d214343b72f04898cf2fb9b6bcf0f6b9
justnothin <vuleanhduc1604@gmail.com>
2022-10-07 19:48:49 +0700

    update project

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
     root: "./src",
     publicDir: "assets",
     build: {
-        outDir: "../dist",
+        outDir: "dist",
         publicDir: "assets",
         rollupOptions: {
             input: {
