cb9cb31d640389a48e9ab7de3b81a5f754957f2e
justnothin <vuleanhduc1604@gmail.com>
2022-10-08 14:29:05 +0700

    update project

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
         publicDir: "assets",
         rollupOptions: {
             input: {
-                trangchu: resolve(__dirname, "src/trangchu.html"),
+                index: resolve(__dirname, "src/index.html"),
                 about: resolve(__dirname, "src/about.html"),
                 products: resolve(__dirname, "src/products.html"),
                 gioithieu: resolve(__dirname, "src/gioithieu.html"),
