ea99f785023c8ccafe9c0c6631bd840acdddd67f
justnothin <vuleanhduc1604@gmail.com>
2022-10-12 22:24:58 +0700

    update project

---
+++ src/js/tracnghiem.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css"
+const circle_1_true = document.querySelector('.circle-1-true');
+const circle_2_true = document.querySelector('.circle-2-true');
+const circle_3_true = document.querySelector('.circle-3-true');
+const circle_4_true = document.querySelector('.circle-4-true');
+const circle_1_false = document.querySelector('.circle-1-false');
+const circle_2_false = document.querySelector('.circle-2-false');
+const circle_3_false = document.querySelector('.circle-3-false');
+const circle_4_false = document.querySelector('.circle-4-false');
+const circle_5 = document.querySelector('.circle-5');
