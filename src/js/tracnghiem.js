a8aefb6cbaba77d95a18b6faebfd4b0a8809a103
justnothin <vuleanhduc1604@gmail.com>
2022-10-14 20:28:06 +0700

    update project

--- src/js/tracnghiem.js
+++ src/js/tracnghiem.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css"
-const circle_1_true = document.querySelector('.circle-1-true');
+ /* const circle_1_true = document.querySelector('.circle-1-true');
 const circle_2_true = document.querySelector('.circle-2-true');
 const circle_3_true = document.querySelector('.circle-3-true');
 const circle_4_true = document.querySelector('.circle-4-true');
@@ -8,3 +8,163 @@ const circle_2_false = document.querySelector('.circle-2-false');
 const circle_3_false = document.querySelector('.circle-3-false');
 const circle_4_false = document.querySelector('.circle-4-false');
 const circle_5 = document.querySelector('.circle-5');
+const question_1 = document.querySelector('.question-1-wrapper');
+const question_2 = document.querySelector('.question-2-wrapper');
+const question_3 = document.querySelector('.question-3-wrapper');
+const question_4 = document.querySelector('.question-4-wrapper');
+const question_5 = document.querySelector('.question-5-wrapper');
+const parent_clas = circle_1_true.closest('.question'); */
+var question_1_point = 0;
+const option_11 = document.querySelector('.num-11');
+const option_12 = document.querySelector('.num-12');
+const option_13 = document.querySelector('.num-13');
+const option_14 = document.querySelector('.num-14');
+const option_15 = document.querySelector('.num-15');
+const option_16 = document.querySelector('.num-16');
+const option_17 = document.querySelector('.num-17');
+const option_18 = document.querySelector('.num-18');
+const option_19 = document.querySelector('.num-19');
+option_11.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_12.addEventListener("click", function() { 
+    question_1_point = 4;
+    console.log(question_1_point)
+});
+option_13.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_14.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_15.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_16.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_17.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_18.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_19.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_21.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_22.addEventListener("click", function() { 
+    question_1_point = 4;
+    console.log(question_1_point)
+});
+option_23.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_24.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_25.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_26.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_27.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_28.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_29.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_11.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_12.addEventListener("click", function() { 
+    question_1_point = 4;
+    console.log(question_1_point)
+});
+option_13.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_14.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_15.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_16.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_17.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_18.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_19.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_11.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_12.addEventListener("click", function() { 
+    question_1_point = 4;
+    console.log(question_1_point)
+});
+option_13.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_14.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_15.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_16.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_17.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_18.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
+option_19.addEventListener("click", function() { 
+    question_1_point = 5;
+    console.log(question_1_point)
+});
